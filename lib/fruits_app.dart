68b748b9741296f82d572c26d1a3b80e4d1e4a25
Mostafa Elsayed <mostafaembaby19@gmail.Com>
2025-11-29 16:51:36 +0200

    - Added splash view images - Create splash view

---
+++ lib/fruits_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub_course/Features/Splash/presentation/views/splash_view.dart';
+
+class FruitsApp extends StatelessWidget {
+  const FruitsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const SplashView());
+  }
+}
